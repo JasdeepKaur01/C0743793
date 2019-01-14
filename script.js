27c86eed7366595ee9207dd045569bea465e3f39
JasdeepKaur01 <46692884+JasdeepKaur01@users.noreply.github.com>
2019-01-14 16:59:26 -0500

    Week 2- Work

---
+++ script.js
@@ -0,0 +1,15 @@
+// Name: Jasdeep Kaur
+// Date: January 14, 2019
+// Student: C0743793
+// Course: CSD 2214 Section 6
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('#clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const minutesSpan = clock.querySelector('.minutes');
+const secondsspan = clock.querySelector('.seconds');
+
+//console.log("Hey Jasdeep");
+//console.log(endDate.value);
